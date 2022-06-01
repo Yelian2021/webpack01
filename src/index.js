90f10b431b7f31d6a6ddb409c86e136db8c71a66
Yelian <ch99881203@gmail.com>
2022-06-01 13:15:06 +0800

    'first'

---
+++ src/index.js
@@ -0,0 +1,19 @@
+var s = 10;
+
+function matsuA(s){
+    return s + 10
+}
+
+console.log(matsuA(20));
+
+//jquery
+import $ from 'jquery';
+
+$('body').css('background-color','#333');
+$('.box').css({
+    'width':'300px',
+    'height':'300px',
+    'background-color':'black',
+    'margin' : '0 auto', 
+    'transition':'.5s',
+});
